a0ca416295bf95fae33fa3051e7aaca0eefb26d3
kimalss22 <kimalss22@gmail.com>
2023-03-20 16:39:29 +0900

    jwt 검증하기 테스트

--- src/test/java/shop/mtcoding/jwtstudy/example/JwtTset.java
+++ src/test/java/shop/mtcoding/jwtstudy/example/JwtTset.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class JwtTset {
     // ABC(메타코딩) -> 1313AB (검증O)
@@ -24,6 +27,32 @@ public class JwtTset {
                 .sign(Algorithm.HMAC512("메타코딩"));
 
         System.out.println("테스트 : " + jwt);
+        // then
+
+    }
+
+    @Test
+    public void verify_test() throws Exception {
+        // given
+        String jwt = JWT.create().withSubject("토큰제목")
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
+                .withClaim("id", 1)
+                .withClaim("role", "manager")
+                .sign(Algorithm.HMAC512("메타코딩"));
+
+        // when
+        try {
+            DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512("메타코딩")).build().verify(jwt);
+            int id = decodedJWT.getClaim("id").asInt();
+            String role = decodedJWT.getClaim("role").asString();
+            System.out.println("테스트 : " + id);
+            System.out.println("테스트 : " + role);
+        } catch (SignatureVerificationException sve) {
+            System.out.println("테스트 : " + sve.getMessage()); // 위조
+        } catch (TokenExpiredException tee) {
+            System.out.println("테스트 : " + tee.getMessage()); // 토큰 기간 완료
+        }
+
         // then
 
     }
